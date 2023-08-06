b0af6e99b8a7c1d53580a593452065a232fa610b
abdussamad26 <assamadansari0000@gmail.com>
2023-08-06 15:00:43 +0530

    initial updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -56,6 +56,54 @@ function Projects() {
                       </div>
                       </div>
                    </div>
+                    <div className='col-lg-3 py-3 d-flex justify-content-center align-items-center'>
+                      <div className='box d-flex justify-content-center align-items-center '>
+                        <div class="stack">
+                          <div class="card cardsam">
+                            <div class="image">
+                            <img style={{height:"100%",width:"100%"}} className='border' src="cart-redux.jpg" alt="images"/>
+                            </div>
+                            <a className='proj-link' href='https://cart-functionality-redux.netlify.app/'><span>view Projects</span></a>
+                          </div>
+                      </div>
+                      </div>
+                   </div>
+                    <div className='col-lg-3 py-3 d-flex justify-content-center align-items-center'>
+                      <div className='box d-flex justify-content-center align-items-center '>
+                        <div class="stack">
+                          <div class="card cardsam">
+                            <div class="image">
+                            <img style={{height:"100%",width:"100%"}} className='border' src="weather-img.jpg" alt="images"/>
+                            </div>
+                            <a className='proj-link' href='https://weather-app-asamad.netlify.app/'><span>view Projects</span></a>
+                          </div>
+                      </div>
+                      </div>
+                   </div>
+                    <div className='col-lg-3 py-3 d-flex justify-content-center align-items-center'>
+                      <div className='box d-flex justify-content-center align-items-center '>
+                        <div class="stack">
+                          <div class="card cardsam">
+                            <div class="image">
+                            <img style={{height:"100%",width:"100%"}} className='border' src="CRUD-img.png" alt="images"/>
+                            </div>
+                            <a className='proj-link' href='https://crud-react-asamad.netlify.app/'><span>view Projects</span></a>
+                          </div>
+                      </div>
+                      </div>
+                   </div>
+                    <div className='col-lg-3 py-3 d-flex justify-content-center align-items-center'>
+                      <div className='box d-flex justify-content-center align-items-center '>
+                        <div class="stack">
+                          <div class="card cardsam">
+                            <div class="image">
+                            <img style={{height:"100%",width:"100%"}} className='border' src="webflix-img.jpg" alt="images"/>
+                            </div>
+                            <a className='proj-link' href='https://rad-gaufre-94dfd8.netlify.app/'><span>view Projects</span></a>
+                          </div>
+                      </div>
+                      </div>
+                   </div>
             </div>
           </div>
         </section>
