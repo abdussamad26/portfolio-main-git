b78b944584dbfd191caaa30e7362b7f2cbd1cc5b
abdussamad26 <assamadansari0000@gmail.com>
2023-08-06 12:26:12 +0530

    updated

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,7 +4,7 @@ import './Social.css'
 function Social() {
   return (
     <div>
-        <section style={{backgroundColor:"#1f1f1f"}}>
+        <section className="mt-5" style={{backgroundColor:"#1f1f1f"}}>
             <div className='container'>
                 <div className='row'>
                     <div className='col-lg-12 text-center'>
